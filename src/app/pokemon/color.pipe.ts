063fe84390fde3068ded790dc4d3da6e9dbec661
tomasgoyret <tomas.goyret@gmail.com>
2022-06-14 13:11:59 -0300

    som color to types

--- src/app/pokemon/color.pipe.ts
+++ src/app/pokemon/color.pipe.ts
@@ -1,21 +1,57 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Pokemon } from './pokemon.interfaces';
+import { PokemonService } from './pokemon.service';
 
 @Pipe({
   name: 'colorBackground'
 })
 export class ColorBackgroundPipe implements PipeTransform {
+  
 
   transform(poke: string): string {
 
-
-    if(poke == "water") {
-      return "blue"
-    } else if (poke == "grass") {
-      return "green"
-    } else {
-      return "yellow"
+    switch(poke) {
+      case "normal":
+        return "red"
+      case "fighting":
+        return "blue"
+      case "flying":
+        return "yellow"
+      case "poison":
+        return "green"
+      case "ground":
+        return "purple"
+      case "rock":
+        return "orange"
+      case "bug":
+        return "dark-green"
+      case "ghost":
+        return "black"
+      case "fire":
+        return "red"
+      case "water":
+        return "blue"
+      case "grass":
+        return "blue"
+      case "electric":
+        return "yellow"
+      case "psychic":
+        return "blue"
+      case "ice":
+        return "blue"
+      case "dragon":
+        return "blue"
+      case "dark":
+        return "blue"
+      case "fairy":
+        return "blue"
+      case "unknown":
+        return "blue"
+      case "shadow":
+        return "blue"
+      default:
+        return "grey"
     }
+
     
   }
 

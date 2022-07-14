6a5be240601b7b77acf608135e2a17a0b572dab8
tomasgoyret <tomas.goyret@gmail.com>
2022-07-14 13:21:25 -0300

    update

--- src/app/pokemon/pokemoncard/pokemoncard.component.ts
+++ src/app/pokemon/pokemoncard/pokemoncard.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Pokemon } from '../pokemon.interfaces';
 
 @Component({
@@ -8,18 +8,28 @@ import { Pokemon } from '../pokemon.interfaces';
 })
 export class PokemoncardComponent implements OnInit {
 
-  @Input() pokecard! : Pokemon;
+  @Input() pokecard!: Pokemon;
+  public isFavourite: boolean = false
 
   constructor(
-    ) {}
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
-  addToFavourite(pokemon: Pokemon){
+  check(){
     let oldiesFav = localStorage.getItem("favs")
     let allFavs = JSON.parse(oldiesFav || "[]")
-    let joinAllWithNew = [...allFavs,pokemon]
+    let exist = allFavs.find((p: Pokemon) => p.id === this.pokecard.id)
+    exist ? this.isFavourite === true : false
+  }
+
+
+  addToFavourite(pokemon: Pokemon) {
+    let oldiesFav = localStorage.getItem("favs")
+    let allFavs = JSON.parse(oldiesFav || "[]")
+    let joinAllWithNew = [...allFavs, pokemon]
     localStorage.setItem("favs", JSON.stringify(joinAllWithNew))
   }
 

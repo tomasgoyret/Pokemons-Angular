3ab2c7292bf764350b27b89da467a3d55d50c21b
tomasgoyret <tomas.goyret@gmail.com>
2022-07-14 11:18:54 -0300

    method to add to favourites

--- src/app/pokemon/pokemoncard/pokemoncard.component.ts
+++ src/app/pokemon/pokemoncard/pokemoncard.component.ts
@@ -10,13 +10,17 @@ export class PokemoncardComponent implements OnInit {
 
   @Input() pokecard! : Pokemon;
 
-  constructor() { }
+  constructor(
+    ) {}
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
   }
 
-  addToFavourite(){
-    alert("Added to favourites")
+  addToFavourite(pokemon: Pokemon){
+    let oldiesFav = localStorage.getItem("favs")
+    let allFavs = JSON.parse(oldiesFav || "[]")
+    let joinAllWithNew = [...allFavs,pokemon]
+    localStorage.setItem("favs", JSON.stringify(joinAllWithNew))
   }
 
 }
